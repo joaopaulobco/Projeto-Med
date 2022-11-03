16d23508b692cbb58c98f1674925c46b5d0b0b13
Joao Paulo <joaopaulo.bco@gmail.com>
2022-11-03 16:56:24 -0300

    backend config

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv/config'); // permite acesso ao arquivo .env
+const express = require('express');
+const app = express();
+
+// banco de dados
+require('./db');
+// configurações
+require('./configs')(app);
+// rotas
+
+// erros
+
+app.use((req, res, next) => {
+  res.status(404).json('Não encontrado!');
+});
+
+require('./error-handling')(app); // importamos e executamos a função já executando ela.
+
+// exportar app
+module.exports = app;
---
+++ db/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const DB_URI = process.env.MONGO_URI;
+
+const connect = async () => {
+  console.log('Aguarde conexão com banco de dados...');
+  try {
+    // caso não exista link no arquivo .env, geramos um erro.
+    if(!DB_URI) {
+      throw new Error('Sem endereço de banco de dados.');
+    }
+
+    const x = await mongoose.connect(DB_URI);
+    const dbName = x.connections[0].name;
+    console.log(`Conectado ao banco: ${dbName}`);
+  } catch (error) {
+    console.log('Falha ao conectar banco de dados:', error);
+    // process.exit() funciona como um CTRL+C no terminal, encerrando o processo.
+    process.exit();
+  }
+}
+
+connect();

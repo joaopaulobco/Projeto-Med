3286ffac5c32a4c0140f5e689d9421b0aba5133f
Joao Paulo <joaopaulo.bco@gmail.com>
2022-11-25 22:50:12 -0300

    ajuste cors

--- configs/index.js
+++ configs/index.js
@@ -8,14 +8,11 @@ module.exports = (app) => {
   app.set('trust proxy', 1);
 
   // caso um endereço não seja especificado, permitiremos acesso apenas a partir do localhost:3000, padrão do REACT.
-  const FRONTEND_URL = process.env.ORIGIN || 'http://localhost:3000';
 
   // configurando CORS para aceitar apenas requisições de um lugar.
   // para deixar aberto à qualquer domínio basta usar: app.use(cors());
   // app.use(cors()); OU app.use(cors({ origin: '*' });
-  app.use(cors({
-    origin: [FRONTEND_URL],
-  }));
+  app.use(cors());
 
   // o logger faz console.logs automáticos, com o método, caminho, status e tempo da resposta.
   app.use(logger('dev'));
